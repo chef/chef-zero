e792ec1eef0e0d004561dded5304abc43e89a246
jkeiser <jkeiser@opscode.com>
2012-12-20 16:47:30 -0800

    Use authentication (chef client needs it for cookbook upload)

--- playground/.chef/knife.rb
+++ playground/.chef/knife.rb
@@ -1,5 +1,5 @@
 chef_repo = File.join(File.dirname(__FILE__), "..")
 chef_server_url "http://127.0.0.1:8889"
-node_name "admin"
-client_key false
+node_name "stickywicket"
+client_key File.join(File.dirname(__FILE__), "stickywicket.pem")
 cookbook_path "#{chef_repo}/cookbooks"
