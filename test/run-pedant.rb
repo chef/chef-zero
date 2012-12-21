2cda610b0111f27ea49010130b92a913eb233541
jkeiser <jkeiser@opscode.com>
2012-12-20 18:05:39 -0800

    Allow latest pedant to be run at any time

--- test/run-pedant.rb
+++ test/run-pedant.rb
@@ -10,8 +10,8 @@ thread = Thread.new do
 end
 
 system('git clone git://github.com/opscode/chef-pedant.git')
-#system('cd chef-pedant && git pull')
-system('cd chef-pedant && git reset --hard 458a3eed89915ff54913040f0001fd2ccd75511b')
+system('cd chef-pedant && git pull')
+#system('cd chef-pedant && git reset --hard 458a3eed89915ff54913040f0001fd2ccd75511b')
 system('cd chef-pedant && bundle install')
 result = system('cd chef-pedant && bin/chef-pedant -c ../chef-zero-pedant-config.rb --skip-validation --skip-authentication --skip-authorization')
 thread.kill
