c7f13a328d03835361fafe53e19b2357291a650c
John Keiser <jkeiser@opscode.com>
2013-04-17 10:06:54 -0700

    Return actual json in 404 (fixes issue with recent cehf bootstrap)

--- lib/chef_zero/endpoints/not_found_endpoint.rb
+++ lib/chef_zero/endpoints/not_found_endpoint.rb
@@ -1,8 +1,10 @@
+require 'json'
+
 module ChefZero
   module Endpoints
     class NotFoundEndpoint
       def call(request)
-        return [404, {"Content-Type" => "application/json"}, "Object not found: #{request.env['REQUEST_PATH']}"]
+        return [404, {"Content-Type" => "application/json"}, JSON.pretty_generate({"error" => ["Object not found: #{request.env['REQUEST_PATH']}"]})]
       end
     end
   end
