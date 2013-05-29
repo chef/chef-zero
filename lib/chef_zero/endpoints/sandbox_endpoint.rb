6fec322a7ee49cc52c6ae32dd971ae3876539d68
John Keiser <jkeiser@opscode.com>
2013-05-29 07:41:59 -0700

    Mimick Chef 11's 503 when some files aren't uploaded

--- lib/chef_zero/endpoints/sandbox_endpoint.rb
+++ lib/chef_zero/endpoints/sandbox_endpoint.rb
@@ -1,4 +1,6 @@
 require 'chef_zero/rest_base'
+require 'chef_zero/rest_error_response'
+require 'json'
 
 module ChefZero
   module Endpoints
@@ -6,6 +8,11 @@ module ChefZero
     class SandboxEndpoint < RestBase
       def put(request)
         existing_sandbox = JSON.parse(get_data(request), :create_additions => false)
+        existing_sandbox['checksums'].each do |checksum|
+          if !exists_data?(request, ['file_store', 'checksums', checksum])
+            raise RestErrorResponse.new(503, "Checksum not uploaded: #{checksum}")
+          end
+        end
         delete_data(request)
         json_response(200, {
           :guid => request.rest_path[1],
