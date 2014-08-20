21d87873e784f97aad21d92eeef9b3a059e16b67
John Keiser <jkeiser@opscode.com>
2014-08-20 10:37:10 -0700

    Get authenticate_user working

--- lib/chef_zero/endpoints/authenticate_user_endpoint.rb
+++ lib/chef_zero/endpoints/authenticate_user_endpoint.rb
@@ -7,18 +7,20 @@ module ChefZero
     class AuthenticateUserEndpoint < RestBase
       def post(request)
         request_json = JSON.parse(request.body, :create_additions => false)
-        name = request_json['name']
+        name = request_json['username']
         password = request_json['password']
         begin
-          user = data_store.get(request.rest_path[0..-2] + ['users', name])
+          user = data_store.get(['users', name])
         rescue ChefZero::DataStore::DataNotFoundError
           raise RestErrorResponse.new(401, "Bad username or password")
         end
         user = JSON.parse(user, :create_additions => false)
-        user = DataNormalizer.normalize_user(user, name, 'username')
+        user = DataNormalizer.normalize_user(user, name, [ 'username' ])
         if user['password'] != password
           raise RestErrorResponse.new(401, "Bad username or password")
         end
+        # Include only particular user data in the response
+        user.keep_if { |key,value| %w(first_name last_name display_name email username).include?(key) }
         json_response(200, {
           'status' => 'linked',
           'user' => user
