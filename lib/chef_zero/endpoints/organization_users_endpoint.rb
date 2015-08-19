4223892e11e9c26cd2e37a254b8e0ca633993227
tyler-ball <tyleraball@gmail.com>
2015-08-19 17:40:00 -0600

    Adding back logic to delete the association request when adding a user to an org (as well as adding the user to the groups)

--- lib/chef_zero/endpoints/organization_users_endpoint.rb
+++ lib/chef_zero/endpoints/organization_users_endpoint.rb
@@ -7,7 +7,32 @@ module ChefZero
     # /organizations/ORG/users
     class OrganizationUsersEndpoint < RestBase
       def post(request)
-        ChefZero::Endpoints::OrganizationUserBase.post(self, request, 'username')
+        orgname = request.rest_path[1]
+        json = FFI_Yajl::Parser.parse(request.body, :create_additions => false)
+        username = json['username']
+
+        if exists_data?(request, [ 'organizations', orgname, 'users', username ])
+          raise RestErrorResponse.new(409, "User #{username} is already in organization #{orgname}")
+        end
+
+        users = get_data(request, [ 'organizations', orgname, 'groups', 'users' ])
+        users = FFI_Yajl::Parser.parse(users, :create_additions => false)
+
+        create_data(request, request.rest_path, username, '{}')
+
+        # /organizations/ORG/association_requests/USERNAME-ORG
+        begin
+          delete_data(request, [ 'organizations', orgname, 'association_requests', username], :data_store_exceptions)
+        rescue DataStore::DataNotFoundError
+        end
+
+        # Add the user to the users group if it isn't already there
+        if !users['users'] || !users['users'].include?(username)
+          users['users'] ||= []
+          users['users'] |= [ username ]
+          set_data(request, [ 'organizations', orgname, 'groups', 'users' ], FFI_Yajl::Encoder.encode(users, :pretty => true))
+        end
+        json_response(201, { "uri" => build_uri(request.base_uri, request.rest_path + [ username ]) })
       end
 
       def get(request)
