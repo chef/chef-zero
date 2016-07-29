ed1c9bff39b3895ae0bb93d73059089bab70b02b
Thom May <thom@chef.io>
2016-07-29 12:17:43 +0100

    Connect to chef zero default port in the playground
    Fixes: #128
    Signed-off-by: Thom May <thom@may.lt>

--- playground/.chef/knife.rb
+++ playground/.chef/knife.rb
@@ -1,6 +1,6 @@
 chef_repo = File.join(File.dirname(__FILE__), "..")
 
-chef_server_url "http://127.0.0.1:4000"
+chef_server_url "http://127.0.0.1:8889"
 node_name       "stickywicket"
 client_key      File.join(File.dirname(__FILE__), "stickywicket.pem")
 cookbook_path   "#{chef_repo}/cookbooks"
